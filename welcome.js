9bd0a6c3c7072ae360fadb8fab2428e5071e8f3c
raj9525 <raj9525@github.com>
2020-09-03 10:59:13 +0530

    window remove

--- welcome.js
+++ welcome.js
@@ -1,7 +1,7 @@
 
-    window.alert("1. Edit texts in the text area & click submit\n2. Choose your image\n3. Drag to adjust the Employee's photo to get exact output"+
+    /*window.alert("1. Edit texts in the text area & click submit\n2. Choose your image\n3. Drag to adjust the Employee's photo to get exact output"+
     "\n4. Click fix button\n5. If you want to adjust again click adjust button and adjust \n6. Once you satisfied with the look then download\n\n"
-    + "***Note: When uploading an image, please make sure the space between employee's head and top edge of the image in atleast 18mm same like below default image" );
+    + "***Note: When uploading an image, please make sure the space between employee's head and top edge of the image in atleast 18mm same like below default image" );*/
 
 
    
